0ad03bb2b6e5a793720714907b21cfcbdc7288df
SSneakySnek <thomasn303@gmail.com>
2024-08-26 20:29:50 +0200

    added Header to Main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import Header from "./components/Header.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Header />
     <App />
   </StrictMode>
 );
