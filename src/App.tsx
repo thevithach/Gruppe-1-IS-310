d9787b01759448a486842a6c7c21374f35897285
Omarp321 <Omarpassi73@gmail.com>
2024-08-26 21:27:10 +0200

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./output.css";
 import Footer from './components/Footer';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
+      <NavBar/>
       <div className="flex-grow">
         {/* Your page content goes here */}
       </div>
