2036bbaf43b3c9e88ebcd2840ee5f38a9c636210
SSneakySnek <thomasn303@gmail.com>
2024-08-28 19:46:27 +0200

    mui: endre header widht

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
 const Header = () => (
-  <div className="relative container mx-auto px-4 py-48 ">
+  <div className="relative px-6 py-48">
     <div className="absolute inset-0 overflow-hidden">
-      <div className="absolute top-0 left-0 w-64 h-64 bg-orange-500 rounded-full opacity-30 transform -translate-x-20 -translate-y-20"></div>
+      <div className="absolute top-0 left-0 w-64 h-64 bg-orange-500 rounded-full opacity-30 transform -translate-x-[7rem] -translate-y-20"></div>
       <div className="absolute top-1/2 left-1/2 w-96 h-64 bg-orange-500 opacity-30 transform -translate-x-16 -translate-y-56 rotate-12"></div>
       <div className="absolute bottom-24 right-0 w-40 h-40 bg-orange-500 rounded-full opacity-30 transform translate-x-16 translate-y-0"></div>
     </div>
 
     <div className="relative flex flex-col md:flex-row items-center justify-between mb-8">
-      <div className="md:w-1/2 mb-4 md:mb-0">
+      <div className="md:w-1/2 md:mb-0 mb-4 pl-20">
         <h1 className="text-5xl font-bold mb-1 text-orange-500">Hei sann!</h1>
         <h2 className="text-2xl text-gray-700">Vi er Gruppe 1</h2>
         <p className="text-lg text-black-600 max-w-screen-sm mt-10 leading-relaxed">
