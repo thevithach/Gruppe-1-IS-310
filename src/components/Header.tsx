4e4a93fc455ff4b7dfa5a620d3c160524ec91206
SSneakySnek <thomasn303@gmail.com>
2024-08-29 19:03:40 +0200

    mui: flex on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,27 @@
-const Header = () => (
-  <div className="relative px-6 py-48">
-    <div className="absolute inset-0 overflow-hidden">
-      <div className="absolute top-0 left-0 w-64 h-64 bg-orange-500 rounded-full opacity-30 transform -translate-x-[7rem] -translate-y-20"></div>
-      <div className="absolute top-1/2 left-1/2 w-96 h-64 bg-orange-500 opacity-30 transform -translate-x-16 -translate-y-56 rotate-12"></div>
-      <div className="absolute bottom-24 right-0 w-40 h-40 bg-orange-500 rounded-full opacity-30 transform translate-x-16 translate-y-0"></div>
-    </div>
 
-    <div className="relative flex flex-col md:flex-row items-center justify-between mb-8">
-      <div className="md:w-1/2 md:mb-0 mb-4 pl-20">
-        <h1 className="text-5xl font-bold mb-1 text-orange-500">Hei sann!</h1>
-        <h2 className="text-2xl text-gray-700">Vi er Gruppe 1</h2>
-        <p className="text-lg text-black-600 max-w-screen-sm mt-10 leading-relaxed">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur
-          sint occaecat cupidatat non proident, sunt in culpa qui officia
-          deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui.
+const Header = () => (
+  <div className="relative md:py-40 mb-16 max-w-screen-max mx-auto">
+    <div className="absolute inset-0 overflow-hidden">
+      <div className="absolute top-0 left-0 w-32 h-32 md:w-64 md:h-64 bg-orange-500 rounded-full opacity-30 transform -translate-x-1/2 -translate-y-1/2"></div>
+      <div className="absolute top-1/2 left-1/2 w-48 h-32 md:w-96 md:h-64 bg-orange-500 opacity-30 transform -translate-x-1/2 -translate-y-1/2 rotate-12"></div>
+      <div className="absolute bottom-0 right-0 w-20 h-20 md:w-40 md:h-40 bg-orange-500 rounded-full opacity-30 transform translate-x-1/2 translate-y-1/2"></div>
+    </div>
+    <div className="relative flex flex-col items-center md:items-stretch md:flex-row justify-between">
+      <div className="w-auto mb-10 pt-10 mx-5 md:w-6/12 md:pr-10 md:pl-24 md:pt-20">
+        <h1 className="text-4xl md:text-5xl font-bold mb-2 text-orange-500">Hei sann!</h1>
+        <h2 className="text-xl md:text-2xl text-gray-700 mb-4">Vi er Gruppe 1</h2>
+        <p className="text-base md:text-lg text-black-600 leading-relaxed mt-4 md:mt-6">
+          Vi er en gruppe på seks IT-studenter som har valgt å samarbeide om bacheloroppgaven. Flere av oss har tidligere jobbet sammen i tidligere semestre,
+          noe som har ført til gode karakterer, godt samhold og samarbeid. Gjennom studiet har vi tilegnet oss solid kompetanse
+          innen systemutvikling og en sterk interesse for IT-systemer. Målet vårt for bacheloroppgaven er å finne en oppdragsgiver som kan utfordre
+          våre ferdigheter og utvide vår kunnskap. Vi er motivert for å lære nye konsepter og ser frem til å ta fatt på en kompleks men spennende oppgave.
         </p>
       </div>
-      <div className="md:w-1/2">
+      <div className="w-auto flex justify-center md:w-5/12">
         <img
           src="./src/assets/group.jpg"
           alt="group-image"
-          className="w-3/4 h-auto rounded-lg shadow-lg mx-36 border-4 border-orange-500"
+          className="w-full max-w-md md:max-w-2xl md:mr-24 h-auto rounded-lg shadow-lg border-4 border-orange-500"
         />
       </div>
     </div>
