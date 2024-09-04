60d2800ca5e11a621aad885a805504edc55e092d
thevithach <vitt@uia.no>
2024-09-04 13:32:12 +0200

    Navigation Fix

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { scrollToProfileSection } from "../../util/scrollUtils";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,19 +9,35 @@ function NavBar() {
     setIsOpen(!isOpen);
   };
 
+  const navigate = useNavigate();
+  const location = useLocation();
+  const handleOmOssClick = () => {
+    navigate("/", { state: { scrollToProfile: true } });
+  };
   return (
     <>
       <nav className="bg-[#fcf8f4] shadow-md fixed top-0 left-0 w-full z-50">
         <div className="max-w-screen-lg flex flex-wrap items-center justify-between mx-auto py-5 lg:px-0 px-4 ">
           {/* Brand Name as a clickable link */}
-          <a
-            href="/"
-            className="flex text-[#181819] hover:text-opacity-65 items-center space-x-3 rtl:space-x-reverse"
-          >
-            <span className="text-2xl font-semibold whitespace-nowrap ">
-              Prosjektgjennomføring
-            </span>
-          </a>
+          {location.pathname === "/HvorforOss" ? (
+            <Link
+              to="/"
+              className="flex text-[#181819] hover:text-opacity-65 items-center space-x-3 rtl:space-x-reverse"
+            >
+              <span className="text-2xl font-semibold whitespace-nowrap ">
+                Prosjektgjennomføring
+              </span>
+            </Link>
+          ) : (
+            <a
+              href="#header-section"
+              className="flex text-[#181819] hover:text-opacity-65 items-center space-x-3 rtl:space-x-reverse"
+            >
+              <span className="text-2xl font-semibold whitespace-nowrap ">
+                Prosjektgjennomføring
+              </span>
+            </a>
+          )}
 
           {/* Button to toggle mobile menu */}
           <button
@@ -56,22 +73,45 @@ function NavBar() {
             id="navbar-default"
           >
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
-              <li>
-                <a
-                  href="#header-section"
-                  className="block py-2 px-3 text-[#181819] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-600 md:p-0"
-                >
-                  Hjem
-                </a>
-              </li>
-              <li>
-                <a
-                  className="block py-2 px-3 text-[#181819] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-600 md:p-0"
-                  onClick={scrollToProfileSection}
-                >
-                  Om oss
-                </a>
-              </li>
+              {location.pathname === "/HvorforOss" ? (
+                <>
+                  <li>
+                    <Link
+                      to="/"
+                      className="block py-2 px-3 text-[#181819] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-600 md:p-0"
+                    >
+                      Hjem
+                    </Link>
+                  </li>
+                  <li>
+                    <button
+                      onClick={handleOmOssClick}
+                      className="block py-2 px-3 text-[#181819] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-600 md:p-0 cursor-pointer"
+                    >
+                      Om oss
+                    </button>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <a
+                      href="#header-section"
+                      className="block py-2 px-3 text-[#181819] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-600 md:p-0"
+                    >
+                      Hjem
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="block py-2 px-3 text-[#181819] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-600 md:p-0 cursor-pointer"
+                      onClick={scrollToProfileSection}
+                    >
+                      Om oss
+                    </a>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,23 @@
 import Header from "../components/Header/Header";
 import About from "../components/About/About";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { scrollToProfileSection } from "../util/scrollUtils";
+
+interface LocationState {
+  scrollToProfile?: boolean;
+}
 
 const Home = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const state = location.state as LocationState;
+    if (state?.scrollToProfile) {
+      scrollToProfileSection();
+    }
+  }, [location]);
+
   return (
     <>
       <Header />
