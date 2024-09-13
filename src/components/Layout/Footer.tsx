97e19c9b63d7c74c73abfacf99fedc2ac728c7ff
Nhan Thomas Trong Nguyen <nt.thom.ng@gmail.com>
2024-09-13 16:48:34 +0200

    refactor footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,39 +2,23 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-[#fcf8f4] text-gray-700 py-6">
-      <div className="container mx-auto text-center">
-        <ul className="list-none flex justify-center gap-4 mb-4">
-          <li>
-            <a
-              href="https://github.com/thevithach/Gruppe-1-IS-310"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i
-                className="fa-brands fa-github"
-                style={{ fontSize: "30px", color: "black" }}
-              ></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i
-                className="fa-brands fa-linkedin"
-                style={{ fontSize: "30px", color: "black" }}
-              ></i>
-            </a>
-          </li>
-        </ul>
-        <p>
-          Issa Al-Musawi, Nhan Thomas Nguyen, Omar Passi, Tony Nguyen Le, Vi The
-          Thach og Viktor Johan Kalhovd
-        </p>
-        <p>© 2024 • Gruppe 1</p>
+    <footer className="bg-[#0e1035] text-gray-300 py-8 shadow-[0_10px_50px_rgba(14,16,53,0.8)]">
+      <div className="container mx-auto text-center md:text-left">
+        <div className="flex flex-col md:flex-row justify-between items-center md:items-start">
+          <div className="mb-4 md:mb-0">
+            <h1 className="font-semibold text-lg">Ta kontakt med oss</h1>
+            <ul className="list-none mt-2 space-y-2">
+              <li className="text-base">
+                Email: <a href="mailto:nt.thom.ng@gmail.com" className="text-gray-300 underline hover:text-orange-500 duration-300">nt.thom.ng@gmail.com</a>
+              </li>
+              <li className="text-base">Nummer: 90620256</li>
+            </ul>
+          </div>
+          <div className="text-sm text-gray-400 text-center md:text-right md:mt-10">
+            <p className="mb-2">Issa Al-Musawi, Nhan Thomas Nguyen, Omar Passi, Tony Nguyen Le, Vi The Thach og Viktor Johan Kalhovd</p>
+            <p>© 2024 • Gruppe 1</p>
+          </div>
+        </div>
       </div>
     </footer>
   );
