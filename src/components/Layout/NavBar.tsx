c1c25ae1f41b3832146650fc00cc0448cc46852d
thevithach <vitt@uia.no>
2024-09-16 19:24:14 +0200

    Bugfix: Update NavBar component link styles in NavBar.tsx

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -136,19 +136,19 @@ function NavBar() {
                     <Link
                       to="/"
                       onClick={scrollToHomeSection}
-                      className="block py-2 px-3 text-white bg-orange-400 rounded md:bg-transparent md:text-orange-400 md:p-0 md:dark:text-orange-400"
+                      className="block py-2 px-3 text-white bg-orange-400 rounded md:bg-transparent md:p-0 md:dark:text-orange-400"
                       aria-current="page"
                     >
                       Hjem
                     </Link>
                   </li>
                   <li>
-                    <button
+                    <a
                       onClick={handleOmOssClick}
-                      className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-orange-400 md:p-0 md:dark:hover:text-orange-400 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                      className="block py-2 px-3 text-gray-900 rounded hover:bg-orange-100 md:hover:bg-transparent md:hover:text-orange-400 md:p-0 md:dark:hover:text-orange-400 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                     >
                       Om oss
-                    </button>
+                    </a>
                   </li>
                 </>
               ) : (
