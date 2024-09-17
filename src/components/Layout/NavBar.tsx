e34e6fc42ad73cb00a14457aa66125cd7d203d91
thevithach <vitt@uia.no>
2024-09-17 22:33:12 +0200

    fix nav items

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -130,7 +130,7 @@ function NavBar() {
             id="navbar-sticky"
           >
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 md:items-center">
-              {location.pathname === "/HvorforOss" ? (
+              {location.pathname !== "/" ? (
                 <>
                   <li>
                     <Link
@@ -149,6 +149,14 @@ function NavBar() {
                     >
                       Om oss
                     </a>
+                  </li>
+                  <li>
+                    <Link
+                      to="/Contact"
+                      className="block py-2 px-3 text-black rounded hover:bg-orange-100 md:hover:bg-transparent md:hover:text-orange-400 md:p-0 md:dark:hover:text-orange-400 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    >
+                      Kontakt
+                    </Link>
                   </li>
                 </>
               ) : (
@@ -169,6 +177,14 @@ function NavBar() {
                     >
                       Om oss
                     </a>
+                  </li>
+                  <li>
+                    <Link
+                      to="/Contact"
+                      className="block py-2 px-3 text-black rounded hover:bg-orange-100 md:hover:bg-transparent md:hover:text-orange-400 md:p-0 md:dark:hover:text-orange-400 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    >
+                      Kontakt
+                    </Link>
                   </li>
                 </>
               )}
