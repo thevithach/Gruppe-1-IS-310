499986e28adf3119707e758a136e7c45fc69d8d5
thevithach <vitt@uia.no>
2024-09-24 19:23:04 +0200

    Refactor: Update button and header styles
    - Update button styles in FormCard component to support dark mode
    - Update header styles in WhyUsBanner component to support dark mode
    - Update card styles in ui components to support dark mode
    - Update input and textarea styles in ui components to support dark mode

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -12,7 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         id={id}
         name={name}
         className={cn(
-          "flex h-9 w-full rounded-md border border-neutral-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300",
+          "flex h-9 w-full rounded-md border border-neutral-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-700 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-800 dark:placeholder:text-neutral-400 dark:focus-visible:ring-neutral-300 dark:bg-white",
           className
         )}
         ref={ref}
